04bb9932cea73fba97ea23d6d80ecf83f62ef005
liviaAugusta <liviamattoni@gmail.com>
2022-07-22 16:29:40 +0200

    creazione del componente Title è bonus

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,22 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MyTitle />
+    <MyHero />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import MyTitle from './components/MyTitle.vue'
+import MyHero from './components/MyHero.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    MyTitle,
+    MyHero
   }
 }
 </script>
---
+++ src/components/MyHero.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="hello">
+    <img src="../assets/PinkFloydHero.jpeg">     
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyHero',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
